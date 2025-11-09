79214a2af876b6b5d4b4df9755873f51bea8979c
tiêu kiệt <tieuminhkiett@gmail.com>
2025-11-09 18:31:11 +0700

    Add files via upload

---
+++ hacker_rank_Set .difference()_Operation.py
@@ -0,0 +1,12 @@
+n1 = int(input())
+student_number_1 = set(map(int,input().split()))
+n2 = int(input())
+student_number_2 = set(map(int,input().split()))
+
+# toán tử - nghĩa là phần chỉ riêng trong 1 tập hợp mà các tập hợp khác không có
+common_part = student_number_1 - student_number_2
+print(common_part)
+
+# hoặc có thể sử dụng .difference() để biểu diễn y hệt như toán tử -
+common_part_2 = student_number_1.difference(student_number_2)
+print(common_part_2)
