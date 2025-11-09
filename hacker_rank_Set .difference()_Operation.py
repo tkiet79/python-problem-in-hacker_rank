9b4f5c81e54ee5df7e1e61eb63a2a556837f60b6
tiêu kiệt <tieuminhkiett@gmail.com>
2025-11-09 18:33:43 +0700

    Update hacker_rank_Set .difference()_Operation.py

--- hacker_rank_Set .difference()_Operation.py
+++ hacker_rank_Set .difference()_Operation.py
@@ -5,8 +5,8 @@ student_number_2 = set(map(int,input().split()))
 
 # toán tử - nghĩa là phần chỉ riêng trong 1 tập hợp mà các tập hợp khác không có
 common_part = student_number_1 - student_number_2
-print(common_part)
+print(len(common_part))
 
 # hoặc có thể sử dụng .difference() để biểu diễn y hệt như toán tử -
 common_part_2 = student_number_1.difference(student_number_2)
-print(common_part_2)
+print(len(common_part_2))
