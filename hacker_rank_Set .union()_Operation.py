3f2d0f639fc2d15bc210af904ce8f0e5398f5e08
tiêu kiệt <tieuminhkiett@gmail.com>
2025-11-09 18:37:38 +0700

    Update hacker_rank_Set .union()_Operation.py

--- hacker_rank_Set .union()_Operation.py
+++ hacker_rank_Set .union()_Operation.py
@@ -4,11 +4,12 @@ student_number_1 = set(map(int,input().split()))
 n2 = int(input())
 student_number_2 = set(map(int,input().split()))
 
-# cách 1: dùng toán tử & nghĩa là phần chung của 2 tập hợp
+# cách 1: dùng toán tử & nghĩa là gộp giá trị của 2 tập hợp lại thành 1
 common_part = student_number_1 & student_number_2
 total = len(student_number_1) + len(student_number_2) - len(common_part)
 print(total)
 
-# cách 2: dùng union() nghĩa là tất cả phần tử không giao nhau của 2 tập hợp
+# cách 2: dùng union() cũng giống như toán tử &
 total_subscribers = student_number_1.union(student_number_2)
+
 print(len(total_subscribers))
